929859ae515a896f307f8ea393eb8851310ff4d1
sendkite <sendkite1@gmail.com>
2022-01-17 17:14:02 +0900

    Feature : create 기능 추가

--- src/main/java/com/example/restspring/persistence/TodoRepository.java
+++ src/main/java/com/example/restspring/persistence/TodoRepository.java
@@ -2,9 +2,13 @@ package com.example.restspring.persistence;
 
 import com.example.restspring.model.TodoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface TodoRepository extends JpaRepository<TodoEntity, String> {
+    @Query("select * from Todo t where t.userId = ?1")  // ?1 은 매개변수 위치 순서
     List<TodoEntity> findByUserId(String userId);
 }
--- src/main/java/com/example/restspring/service/TodoService.java
+++ src/main/java/com/example/restspring/service/TodoService.java
@@ -6,6 +6,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Slf4j
 @Service
 public class TodoService {
@@ -17,8 +19,27 @@ public class TodoService {
 
         TodoEntity entity = TodoEntity.builder().title("My first todo item").build();
         repository.save(entity);
-        TodoEntity savedEntiry = repository.findById(entity.getId()).get();
+        TodoEntity savedEntiry = repository.findById(entity.getId()).get(); // .get()의 경우 결과값이 null일 경우 NoSuchElementException 발생! orElse로 예외 넣어줘야함
         return savedEntiry.getTitle();
 
+    }
+
+    public List<TodoEntity> create(final TodoEntity entity){
+        // 검증, save, return 세 단계로 구성
+        // 1. validation
+        if (entity == null) {
+            log.warn("Entity can not be null");
+        }
+
+        if (entity.getUserId() == null) {
+            log.warn("Unknown user");
+            throw new RuntimeException("Unkown user");
+        }
+
+        // 2. save
+        log.info("Entity Id : {} is saved", entity.getUserId());
+
+        // 3. return
+        return repository.findByUserId(entity.getUserId());
     }
 }
